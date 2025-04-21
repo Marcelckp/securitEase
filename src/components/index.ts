26b63c3b2ee78e7d6f289c302fd0638996324a93
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2025-04-21 21:08:50 +0100

    chore: created main component local export file

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { WeatherDisplay, WeatherEntity } from "./atoms";
+export { HotKeyDialog } from "./molecules";
+export {
+  SnowWeatherVisual,
+  SunWeatherVisual,
+  RainWeatherVisual,
+  CloudWeatherVisual,
+} from "./meshes";
